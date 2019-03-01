5642909852f66d168e1f3267050a2c948139d791
Melvin Cornelissen <melvin.cornelissen@coolblue.nl>
2019-03-01 14:01:56 +0100

    New Blog Build (Fri Mar  1 14:01:56 CET 2019)

---
+++ content/posts/jamf-vbox-win10.md
@@ -0,0 +1,14 @@
+---
+title: "Jamf Self Service and Win10 VMs"
+date: 2019-03-01T13:57:04+01:00
+toc: false
+images:
+tags:
+  - bash
+  - scripts
+  - jamf
+---
+
+### Auto install Win 10 VBox VM's through Jamf self service!
+
+Since we're supporting lots of developers at Coolblue which like to work on a Mac but need to develop for Windows we're enabling them with auto provisioned VM's.
--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,17 @@ hugo -d ../coolbluemelvin.github.io
 echo -e "\033[0;32mChanging to blog directory...\033[0m"
 cd ../coolbluemelvin.github.io
 
-echo -e "\033[0;32mCommit and push the new build...\033[0m"
+echo -e "\033[0;32mCommit and push the new build to coolbluemelvin.github.io...\033[0m"
 git add .
 git commit -am "New Blog Build (`date`)"
 git push
 
 echo -e "\033[0;32mChange back to mlvn.io...\033[0m"
 cd ../mlvn.io
+
+echo -e "\033[0;32mCommit and push the new build to mlvn.io...\033[0m"
+git add -A 
+git commit -am "New Blog Build (`date`)"
+git push
 
 echo -e "\033[0;32mDeploy complete.\033[0m"
